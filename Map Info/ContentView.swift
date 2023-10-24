852523a1be8c091d9daf568532ece744dc660ea4
artym125 <otapik125@gmail.com>
2023-10-24 17:22:48 +0300

    Initial Commit

---
+++ Map Info/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Map Info
+//
+//  Created by Ostap Artym on 24.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Map Info/Map_InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_InfoApp.swift
+//  Map Info
+//
+//  Created by Ostap Artym on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Map_InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
