f27c1cfa67a3fe31da6ecf4a7f51baa3537e3437
artym125 <otapik125@gmail.com>
2023-10-24 17:50:15 +0300

    first step

--- Map Info/ContentViewModel.swift
+++ Map Info/ContentViewModel.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import MapKit
+
+class ContentViewModel: ObservableObject, CLLocationManagerDelegate {
+    
+    var locationManager:CLLocationManager?
+    var searchTerm = ""
+    
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 0, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+    
+    @Published private var places = [Place]()
+    @Published private var selectedPlace: Place?
+    
+    var selectedPlaceName: String {
+        guard let selectPlace = selectedPlace else { return ""}
+        
+        return selectPlace.name
+    }
+    
+    var selectedPlaceAdress: String {
+        guard let selectPlace = selectedPlace else { return ""}
+        if let street = selectedPlace?.placemark.thoroughfare,
+           let subAdrees = selectedPlace?.placemark.subThoroughfare {
+            
+            return street + " " + subAdrees
+        }
+        return ""
+    }
+    
+    //1 - перевірка доступу до локації
+    
+    func checkLocationIsEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager?.delegate = self
+        } else {
+            print("alert")
+        }
+    }
+    
+    //2 - перевірка прав
+    
+    //3 - пошук
+    
+    //4 - вибір місця
+    
+}
--- Map Info/ResultModel.swift
+++ Map Info/ResultModel.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import MapKit
+
+struct Place: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    var placemark: MKPlacemark
+    var coordinate: CLLocationCoordinate2D
+    var adress: String?
+}
